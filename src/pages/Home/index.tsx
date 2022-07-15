dbf0cecc6412f25f975a280e7d4d83c63046dc1d
Matheus Santos <matheus.souza.3@globalhitss.com.br>
2022-07-15 01:47:24 -0300

    feat(FIELO-01): create requeste get image from Bing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { FC, useState, useEffect } from "react";
+import Axios from "axios";
 
 import HTTPClient from "@config/request";
 
@@ -18,24 +19,37 @@ import "./styles.css";
 
 const HomePage: FC = () => {
   const [listMembers, setListMembers] = useState<IMemberProps[]>();
-  const [listActivitiesMember, setListActivitiesMember] = useState<IActivityProps[]>();
+  const [listActivitiesMember, setListActivitiesMember] =
+    useState<IActivityProps[]>();
   const [memberActive, setMemberActive] = useState<IMemberProps | null>();
   const [memberPrograms, setMemberPrograms] = useState<IMemberProgramsProps>();
   const [levelMember, setLevelMember] = useState<ILevelMemberProps>();
   const [nextLevel, setNextLevel] = useState<ILevelMemberProps>();
   const [cardsDetailsMember, setCardsDetailsMember] = useState<boolean>(false);
+  const [imageURL, setImageURL] = useState<string>();
+
+  const getImageBackground = async () => {
+    try {
+      const { data } = await Axios.get(
+        "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR"
+      );
+      setImageURL(`https://www.bing.com${data.images[0].url}`)
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const getListMembers = async () => {
     try {
       const { data } = await HTTPClient.get("/users");
       setListMembers(data);
-
     } catch (e) {
       console.log(e);
     }
   };
 
   useEffect(() => {
+    getImageBackground();
     getListMembers();
   }, []);
 
@@ -45,7 +59,6 @@ const HomePage: FC = () => {
       setMemberActive(data);
       getProgramsMemberByProgramID(data.programId);
       getLevelMemberByID(data.levelId);
-
     } catch (e) {
       console.log(e);
     }
@@ -55,32 +68,34 @@ const HomePage: FC = () => {
     try {
       const { data } = await HTTPClient.get(`/programs/${programId}`);
       setMemberPrograms(data);
-
     } catch (e) {
       console.log(e);
     }
   };
-  
+
   const getLevelMemberByID = async (levelId: string) => {
     try {
       const { data } = await HTTPClient.get(`/levels/${levelId}`);
       getProgramsMemberByID(data, data.programId);
-      
     } catch (e) {
       console.log(e);
     }
   };
-  
-  const getProgramsMemberByID = async (dataLevelMember: ILevelMemberProps, id: string) => {
+
+  const getProgramsMemberByID = async (
+    dataLevelMember: ILevelMemberProps,
+    id: string
+  ) => {
     try {
       const { data } = await HTTPClient.get(`/programs/${id}/levels`);
       if (dataLevelMember) {
-        setLevelMember(dataLevelMember)
-        const dataNextLevel = data.filter((item: any) => item.order > dataLevelMember.order)
-        setNextLevel(dataNextLevel[0])
+        setLevelMember(dataLevelMember);
+        const dataNextLevel = data.filter(
+          (item: any) => item.order > dataLevelMember.order
+        );
+        setNextLevel(dataNextLevel[0]);
         setCardsDetailsMember(true);
       }
-
     } catch (e) {
       console.log(e);
     }
@@ -89,13 +104,12 @@ const HomePage: FC = () => {
   const getActivitiesMemberByID = async (id: string) => {
     try {
       const { data } = await HTTPClient.get(`/users/${id}/activities`);
-      console.log(data, 'activities')
-      setListActivitiesMember(data)
+      console.log(data, "activities");
+      setListActivitiesMember(data);
     } catch (e) {
       console.log(e);
     }
   };
-  
 
   const handleSelectMember = (member: IMemberProps) => {
     if (memberActive && member.id === memberActive.id) {
@@ -111,7 +125,12 @@ const HomePage: FC = () => {
     <div className="container-home">
       <HeaderComponent />
 
-      <div className="container-cards-home">
+      <div
+        style={{
+          backgroundImage: `url(${imageURL})`,
+        }}
+        className="container-cards-home"
+      >
         <div className="content-cards-home">
           <CardListMembers
             listMembers={listMembers}
@@ -128,7 +147,11 @@ const HomePage: FC = () => {
             />
           )}
 
-          {cardsDetailsMember && <CardActivityFeedMember listActivitiesMember={listActivitiesMember} />}
+          {cardsDetailsMember && (
+            <CardActivityFeedMember
+              listActivitiesMember={listActivitiesMember}
+            />
+          )}
         </div>
       </div>
     </div>
